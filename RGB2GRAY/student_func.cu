82df01fc74c31a5cce583ec081b1681cd17b794e
haderalim <hader_alim_299@yahoo.com>
2018-03-11 17:41:24 +0200

    Create student_func.cu

---
+++ RGB2GRAY/student_func.cu
@@ -0,0 +1,34 @@
+#include "reference_calc.cpp"
+#include "utils.h"
+#include <stdio.h>
+
+__global__
+void rgba_to_greyscale(const uchar4* const rgbaImage,
+                       unsigned char* const greyImage,
+                       int numRows, int numCols)
+{
+  //TODO
+  //Fill in the kernel to convert from color to greyscale
+  //the mapping from components of a uchar4 to RGBA is:
+  // .x -> R ; .y -> G ; .z -> B ; .w -> A
+  //
+  //The output (greyImage) at each pixel should be the result of
+  //applying the formula: output = .299f * R + .587f * G + .114f * B;
+  //Note: We will be ignoring the alpha channel for this conversion
+
+  //First create a mapping from the 2D block and grid locations
+  //to an absolute 2D location in the image, then use that to
+  //calculate a 1D offset
+}
+
+void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
+                            unsigned char* const d_greyImage, size_t numRows, size_t numCols)
+{
+  //You must fill in the correct sizes for the blockSize and gridSize
+  //currently only one block with one thread is being launched
+  const dim3 blockSize(1, 1, 1);  //TODO
+  const dim3 gridSize( 1, 1, 1);  //TODO
+  rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
+  
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+}
