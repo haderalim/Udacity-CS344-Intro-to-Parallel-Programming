7d17f8c521a73525b7cc8b8c1a9cd5947a7ba213
haderalim <hader_alim_299@yahoo.com>
2018-03-11 17:44:18 +0200

    Create reference_calc.cpp

---
+++ RGB2GRAY/reference_calc.cpp
@@ -0,0 +1,20 @@
+
+// for uchar4 struct
+#include <cuda_runtime.h>
+
+
+void referenceCalculation(const uchar4* const rgbaImage,
+                          unsigned char *const greyImage,
+                          size_t numRows,
+                          size_t numCols)
+{
+  for (size_t r = 0; r < numRows; ++r) 
+  {
+    for (size_t c = 0; c < numCols; ++c) 
+    {
+      uchar4 rgba = rgbaImage[r * numCols + c];
+      float channelSum = .299f * rgba.x + .587f * rgba.y + .114f * rgba.z;
+      greyImage[r * numCols + c] = channelSum;
+    }//end of second for loop
+  }//end of first for loop
+}//end of referenceCalculation method
